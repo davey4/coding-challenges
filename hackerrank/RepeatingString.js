fa6cb355b2d78128a114dfbd006815b9d4fd8959
David Martel <daveymartel@gmail.com>
2021-03-01 14:10:36 -0500

    repeating string

---
+++ hackerrank/RepeatingString.js
@@ -0,0 +1,30 @@
+// // https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+// There is a string, , of lowercase English letters that is repeated infinitely many times. Given an integer, , find and print the number of letter a's in the first  letters of the infinite string.
+
+// Example
+
+// The substring we consider is , the first  characters of the infinite string. There are  occurrences of a in the substring.
+
+// Function Description
+
+// Complete the repeatedString function in the editor below.
+
+// repeatedString has the following parameter(s):
+
+// s: a string to repeat
+// n: the number of characters to consider
+// Returns
+
+// int: the frequency of a in the substring
+
+function repeatedString(s, n) {
+  let repeated = s.repeat(n);
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    if (repeated[i] === "a") {
+      count++;
+    }
+  }
+  return count;
+}
