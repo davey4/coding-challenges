715a91a2bdfbc65f9a953ea3ff83082d15909f48
David Martel <daveymartel@gmail.com>
2021-03-05 09:41:02 -0500

    arrays left rotation

---
+++ hackerrank/ArraysLeftRotation.js
@@ -0,0 +1,25 @@
+// // https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+// A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become . Note that the lowest index item moves to the highest index in a rotation. This is called a circular array.
+
+// Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+// Function Description
+
+// Complete the function rotLeft in the editor below.
+
+// rotLeft has the following parameter(s):
+
+// int a[n]: the array to rotate
+// int d: the number of rotations
+// Returns
+
+// int a'[n]: the rotated array
+
+function rotLeft(a, d) {
+  for (let i = 0; i < d; i++) {
+    let temp = a.shift();
+    a.push(temp);
+  }
+  return a;
+}
