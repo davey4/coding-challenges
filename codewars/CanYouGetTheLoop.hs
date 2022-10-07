043409e1e0ca42516356326de553ba31008f4059
David Martel <daveymartel@gmail.com>
2022-10-07 09:56:01 -0400

    Can you get the loop

---
+++ codewars/CanYouGetTheLoop.hs
@@ -0,0 +1,9 @@
+-- https://www.codewars.com/kata/52a89c2ea8ddc5547a000863/haskell
+
+-- You are given a node that is the beginning of a linked list. This list always contains a tail and a loop. Your objective is to determine the length of the loop.
+
+-- For example in the following picture the tail's size is 3 and the loop size is 12:
+
+
+-- -- use the `next :: Node a -> Node a` function to get the following node
+
